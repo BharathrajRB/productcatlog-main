81ee1597554fd08cd56f12efcbffbc0bb6a7fa00
BharathrajRB <bharathrajrb20it@gmail.com>
2024-01-25 10:18:24 +0530

    c

--- productmanagement/src/main/java/com/example/productmanagement/repository/CartItemRepository.java
+++ productmanagement/src/main/java/com/example/productmanagement/repository/CartItemRepository.java
@@ -3,7 +3,6 @@ package com.example.productmanagement.repository;
 import java.util.List;
 import java.util.Optional;
 
-import javax.swing.text.html.Option;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
